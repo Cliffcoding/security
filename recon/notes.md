9fa1211d9dbb00440966d04c986d6bed25b08157
jlclifton0001 <usacys@local>
2021-11-03 11:11:57 -0400

    Notes

---
+++ recon/notes.md
@@ -0,0 +1,32 @@
+
+
+
+##Domain/IP
+
+###DNS reg, IP Address assign
+
+
+#Pen testing
+## phase1: Mission definition
+### goals and targets
+### Scope
+### RoE
+
+## phase2: recon
+#recon
+#open source
+##Web
+### Cached Content, analytics, Proxy Web App, CLI
+##sensitive
+### business data, profiles, non-profit/charity, business filings, historical
+##Public
+### Address, phone, email
+##Social media
+
+## phase3: Footprinting
+## phase4: Exploit and initial access
+
+## Phase 5: Post-exploit
+## Phase6: Documenttaion
+
+
---
+++ recon/notes.sh
@@ -0,0 +1,51 @@
+
+## phase1: Mission definition
+### goals and targets
+### Scope
+### RoE
+
+## Phase2: recon
+# Recon
+#open source
+##Web
+### Cached Content, analytics, Proxy Web App, CLI
+##sensitive
+### business data, profiles, non-profit/charity, business filings, historical
+##Public
+### Address, phone, email
+##Social media
+
+##Domain/IP
+###DNS reg, IP Address assign
+## phase3: Footprinting
+## phase4: Exploit and initial access
+
+## Phase 5: Post-exploit
+## Phase6: Documenttaion
+
+
+## Exploit Research
+	* recon to weapon
+	* pair vuln to exploit
+	* leverage intel/data of network
+	* Align exploit to operation obj
+	* 
+
+#### Linux commands
+
+*version number*
+
+```bash cat /etc/0s-release```
+
+```bash lsb_release -a```
+
+*kernel version*
+
+```bash uname -a```
+
+*OS and op system*
+
+```bash hostnamectl
+
+nmap -o
+```
